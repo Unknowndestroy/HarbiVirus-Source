3ba132008a10d04548dc167a305d4dfabcfcf86e
Unknowndestroy <149819513+Unknowndestroy@users.noreply.github.com>
2025-02-01 00:26:32 +0300

    HV-X-CORE-DEMO Edition uploaded.

---
+++ start1.bat
@@ -0,0 +1,19 @@
+@echo off
+:: C:\data klasörünü oluşturur
+if not exist "C:\data" (
+    mkdir "C:\data"
+    echo C:\data klasörü başarıyla oluşturuldu.
+) else (
+    echo C:\data klasörü zaten mevcut.
+)
+
+:: Bulunduğunuz klasördeki yalnızca dosyaları kopyalar, down.py ve start1.bat hariç
+echo Dosyalar kopyalanıyor...
+for %%f in (*.*) do (
+    if /i not "%%~nxf"=="down.py" if /i not "%%~nxf"=="start1.bat" (
+        xcopy "%%f" "C:\data\" /y
+    )
+)
+
+
+start START.py
